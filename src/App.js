7d60f65664eda69c9ff64b6aff7d1455f910998b
Gorka <gorka@aquigorka.com>
2017-07-08 19:08:24 +0200

    Adds reset data; Adds functionality to add new items

--- src/App.js
+++ src/App.js
@@ -15,6 +15,24 @@ class App extends Component {
     firebase.database().ref().on('value', (snapshot) => {
       console.log('Received update: ', snapshot.val());
       this.setState({ data: snapshot.val(), synching: false })
+      /*
+      firebase.database().ref().set({
+        version: '1.0.0',
+        items: [
+          { id: '001', label: 'Begin packing', priority: -1, status: false },
+          { id: '002', label: 'Book professional movers', priority: -1, status: false },
+          { id: '003', label: 'Prepare the house', priority: -1, status: false },
+          { id: '004', label: 'Review moving plans', priority: -1, status: false },
+          { id: '005', label: 'Prepare for payment', priority: -1, status: false },
+          { id: '006', label: 'Pack an essentials box', priority: -1, status: false },
+          { id: '007', label: 'Prepare appliances', priority: -1, status: false },
+          { id: '008', label: 'Measure furniture and doorways', priority: -1, status: false },
+          { id: '009', label: 'Do a change of address', priority: -1, status: false },
+          { id: '010', label: 'Take measurements', priority: -1, status: false },
+          { id: '011', label: 'Order supplies', priority: -1, status: false }
+        ]
+      })
+      */
     });
   }
  
@@ -31,21 +49,23 @@ class App extends Component {
     if (synching) {
       return <div>Loading</div>
     }
+    const { items } = data
+    const options = items.filter(x => x.priority === -1)
     return (
       <div className="app">
         <Add
           view={view}
-          data={data}
+          items={options}
           onUpdate={this.onUpdate}
           onShowHome={() => this.setState({ view: 'home' })} />
         <Home
           view={view}
-          data={data}
+          items={items}
           onupdate={this.onupdate}
           onShowAdd={() => this.setState({ view: 'add'})} />
         <Search
           view={view}
-          data={data} />
+          data={items} />
       </div>
     )
   }
--- src/add/index.js
+++ src/add/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import classnames from 'classnames'
 import './styles.css'
 
 const Header = (props) => {
@@ -11,18 +12,63 @@ const Header = (props) => {
   )
 }
 
-const List = () => {
+class Item extends Component{
+ 
+  state = { status: false }
+
+  render() {
+    const { item, onAdd } = this.props
+    const { label, id } = item
+    const { status } = this.state
+    // label
+    const labelClass = classnames('label', { inactive: status })
+    // icon
+    const iconClass = classnames('icon', { close: status })
+    // type
+    const typeClass = classnames('type', { active: status })
+    return (
+      <div className="wrapper"> 
+        <div className={labelClass}>{label}</div>
+        <div className={typeClass}>
+          <div className="high" onClick={() => onAdd(id, 3)}>High Priority</div>
+          <div className="normal" onClick={() => onAdd(id, 2)}>Normal Priority</div>
+          <div className="low" onClick={() => onAdd(id, 1)}>Low Priority</div>
+        </div>
+        <div className={iconClass} onClick={() => this.setState({ status: !status})}>+</div>
+      </div>
+    )
+  }
+}
+
+const List = (props) => {
+  const { items, onAdd } = props
   return (
-    <div>List</div>
+    <ul>
+    {items.sort((a, b) => a.id - b.id).map((item, index) => {
+      const { label, id } = item
+      return (
+        <li key={id}>
+          <Item item={item} onAdd={onAdd} />
+        </li>
+      )
+    })}
+    </ul>
   )
 }
 
 export default (props) => {
-  const { onShowHome } = props
+  const { items = [], onShowHome, onUpdate } = props
   return (
     <section className="add">
       <Header onShowHome={onShowHome} />
-      <List />
+      <List items={items} onAdd={(id, newPriority) => {
+        const item = items.find(x => x.id === id)
+        const { priority, ...rest } = item
+        const newItem = { priority: newPriority, ...rest }
+        const newItems = items.filter(x => x.id !== id)
+          .concat([newItem])
+        onUpdate({ items: newItems })
+      }} />
     </section>
   )
 }
